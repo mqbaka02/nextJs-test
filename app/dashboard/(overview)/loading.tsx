4240fad7ff46cadd6bcb7651a57129252c2a71fd
mqbaka\mqbaka <mqbaka@gmail.com>
2024-11-17 12:03:27 +0300

    Started using streaming for more fluid user exprerience.

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,14 @@
+import DashboardSkeleton from "../../ui/skeletons";
+
+export default function Loading() {
+    // return <div>Loading...</div>;
+    return <DashboardSkeleton/>;
+}
+
+// export default function Loading() {
+//     return <div className="flex items-center justify-center h-screen">
+//     <div className="p-4">
+//         Loading...
+//     </div>
+// </div>;
+// }
