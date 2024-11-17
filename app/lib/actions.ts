4cc4fa00fd9bb2cd54beaf7f81fae411deb9a061
mqbaka\mqbaka <mqbaka@gmail.com>
2024-11-17 22:34:56 +0300

    Try using database when online.

---
+++ app/lib/actions.ts
@@ -0,0 +1,35 @@
+'use server';
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const formSchema= z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string()
+});
+
+const CreateInvoice= formSchema.omit({id: true, date: true});
+
+export default async function createInvoice(formdata: FormData) {
+    const rawFormData= {
+        customerId: formdata.get('customerId'),
+        amount: formdata.get('amount'),
+        status: formdata.get('status'),
+    };
+    const {customerId, amount, status}= CreateInvoice.parse(rawFormData);
+    const amountInCents= amount * 100;
+    const date= new Date().toISOString().split('T')[0];
+    // console.log(rawFormData);
+
+    await sql `
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amount}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashbord/invoices');
+    redirect('/dashbord/invoices');
+}
